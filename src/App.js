dadd9e6b8e06e5288ef93905853d3b2ec091708d
Tamir Yashin <77006200+StartAgain123@users.noreply.github.com>
2022-04-19 19:14:17 +0300

    added geo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter,Route, Routes} from 'react-router-dom'
 import Signup from './Component/Signup/Signup'
 import SignIn from "./Component/SignIn/SignIn";
 import Chat from "./Component/Chat/Chat";
+import Geo from "./Component/Geo/Geo"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
               <Route path="/signup" element={<Signup/>}/>
               <Route path="/signin" element={<SignIn/>}/>
               <Route path="/chat" element={<Chat/>}/>
+              <Route path="/geo" element={<Geo/>}/>
           </Routes>
           </BrowserRouter>
 
